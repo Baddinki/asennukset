77b4a60468fadedec4c5c9bf4c8210105ddfcf13
Mikko Knutas <mikko.knutas@myy.haaga-helia.fi>
2017-11-02 16:30:20 +0000

    uusimmat päivitykset

--- asennukset.sh
+++ asennukset.sh
@@ -1,1 +1,19 @@
-testi
+echo "***************************"
+echo " "
+echo "Moi Mikko"
+echo " "
+echo "***************************"
+sudo timedatectl set-timezone Europe/Helsinki
+setxkbmap fi
+sudo apt-get update
+sudo apt-get install -y git tree puppet
+
+git clone https://github.com/Baddinki/knutas_puppet.git
+
+ sudo puppet apply --modulepath ~/knutas_puppet/puppet/modules/ -e 'class {"ssh":}'
+
+echo "***************************"
+echo " "
+echo "Ready steady go!"
+echo " "
+echo "***************************"
